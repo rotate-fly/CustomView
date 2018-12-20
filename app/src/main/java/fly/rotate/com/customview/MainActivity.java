ccaeed7b305808a574f5229c683d890782c9664c
huangzisheng <huangzs@aheading.com>
2018-12-20 17:56:30 +0800

    【圆环】自定义圆环

--- app/src/main/java/fly/rotate/com/customview/MainActivity.java
+++ app/src/main/java/fly/rotate/com/customview/MainActivity.java
@@ -3,11 +3,21 @@ package fly.rotate.com.customview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fly.rotate.com.customview.progressBar.CircleBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CircleBarView mCircleBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mCircleBarView = findViewById(R.id.circle_view);
+        mCircleBarView.setProgressNum(3000);//设置动画时间为3000毫秒，即3秒
     }
 }
---
+++ app/src/main/java/fly/rotate/com/customview/progressBar/CircleBarView.java
@@ -0,0 +1,94 @@
+package fly.rotate.com.customview.progressBar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class CircleBarView extends View {
+
+    private Paint rPaint;//绘制矩形的画笔
+    private Paint progressPaint;//绘制圆弧的画笔
+
+    private float sweepAngle;//圆弧经过的角度
+
+    private CircleBarAnim anim;
+
+    public CircleBarView(Context context) {
+        this(context, null);
+    }
+
+    public CircleBarView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CircleBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        rPaint = new Paint();
+        rPaint.setStyle(Paint.Style.STROKE);//只描边，不填充
+        rPaint.setColor(Color.RED);
+
+        progressPaint = new Paint();
+        progressPaint.setStyle(Paint.Style.STROKE);//只描边，不填充
+        progressPaint.setColor(Color.BLUE);
+        progressPaint.setStrokeWidth(20);
+        progressPaint.setAntiAlias(true);//设置抗锯齿
+
+        anim = new CircleBarAnim();
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        float x = 50;
+        float y = 50;
+        RectF rectF = new RectF(x, y, x + 300, y + 300);//建一个大小为300 * 300的正方形区域
+
+//        canvas.drawArc(rectF,0,270,false,progressPaint);//这里角度0对应的是三点钟方向，顺时针方向递增
+        canvas.drawRect(rectF, rPaint);
+        canvas.drawArc(rectF, 0, sweepAngle, false, progressPaint);
+    }
+
+    public class CircleBarAnim extends Animation {
+
+        public CircleBarAnim() {
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            super.applyTransformation(interpolatedTime, t);
+            sweepAngle = interpolatedTime * 360;
+            postInvalidate();
+        }
+    }
+
+    /**
+     * 写个方法给外部调用，用来设置动画时间
+     *
+     * @param time
+     */
+    public void setProgressNum(int time) {
+        anim.setDuration(time);
+        this.startAnimation(anim);
+    }
+
+}
